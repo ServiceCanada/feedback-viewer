a3d771066313db700ec832d248febfa62b51b707
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2026-05-04 13:39:58 -0400

    chore: port non-infrastructure changes from aws2 branch
    - Update .gitignore
    - Update pom.xml dependencies
    - Remove scripts/helm/deployMongoDB.sh and values.yml
    - Update CacheConfig, WebSecurityConfig
    - Remove PageConfig
    - Update BadWordController, LoginController, UserController
    - Remove ReportController
    - Add GcIpFilter and GcIpValidationService for GC IP whitelisting
    - Update LanguageFilter, JWTFilter
    - Update CustomTopTaskRepository and Impl
    - Remove EmailService
    - Update ProblemCacheService
    - Add badwords_en.txt, badwords_fr.txt
    - Update topTaskSurvey_styles.css
    - Update department_datalist.html, pageFeedbackDashboard templates
    - Remove reports.html
    - Update topTaskSurvey_en.html and topTaskSurvey_fr.html

--- src/main/java/ca/gc/tbs/repository/CustomTopTaskRepository.java
+++ src/main/java/ca/gc/tbs/repository/CustomTopTaskRepository.java
@@ -2,6 +2,7 @@ package ca.gc.tbs.repository;
 
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.data.mongodb.core.query.Criteria;
 
 public interface CustomTopTaskRepository {
